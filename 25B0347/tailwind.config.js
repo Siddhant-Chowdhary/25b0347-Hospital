8a63b2d693909182f2bf21ceca41b73e044e8ac8
Siddhant-Chowdhary <sidchow25@gmail.com>
2026-07-07 03:31:36 +0530

    Update tailwind.config.js

--- 25B0347/tailwind.config.js
+++ 25B0347/tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,html}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
-        hospital: { 50: '#f0f9ff', 100: '#e0f2fe', 600: '#0284c7', 700: '#0369a1', 900: '#0c4a6e' }
+        hospital: {
+          50: '#f0f9ff',
+          100: '#e0f2fe',
+          600: '#0284c7',
+          700: '#0369a1',
+          900: '#0c4a6e'
+        }
       }
     }
   },
